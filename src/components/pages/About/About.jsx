8ed895ea1e7733e5422a148bff25838c7095e3d4
yash777u <yogeshkumardagar7@gmail.com>
2024-06-18 18:06:54 +0530

    Added social links

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -7,30 +7,55 @@ const About = () => {
   return (
     <div className="h-full page-shadow bg-white p-10">
       {/* heading */}
-      <Heading
-        title="About"
-        subtitle="Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet."
-      />
+      <Heading title="About" subtitle="" />
 
       {/* Details section  */}
 
       <div className="mt-10 text-slate-500">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae
-          quaerat ex obcaecati tenetur dolorum quasi rem! Vero consequuntur
-          perferendis architecto.
+          Postgraduate Student in MCA specializing in Spring Boot, React.js,
+          Hibernate, Git, and REST APIs. I excel in building secure and
+          efficient Java applications, leveraging strong skills in data
+          structures and algorithms. I'm eager to apply my expertise in
+          real-world projects and contribute to innovative solutions. Let's
+          connect and explore opportunities together!
         </p>{" "}
         <br />
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim,
-          beatae.adipisicing elit. Enim, beatae.
-        </p>
+        <p></p>
         {/* Social Links */}
         <div className="flex space-x-4 mt-10">
-          <MdFacebook className="social-btn" />
-          <AiFillInstagram className="social-btn" />
-          <AiFillGithub className="social-btn" />
-          <AiFillLinkedin className="social-btn" />
+          <a
+            href="https://www.facebook.com/your-facebook-profile"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-btn"
+          >
+            <MdFacebook />
+          </a>
+          <a
+            href="https://www.instagram.com/your-instagram-profile"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-btn"
+          >
+            <AiFillInstagram />
+          </a>
+          <a
+            href="https://github.com/yash777u"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-btn"
+          >
+            <AiFillGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/yogesh-kumar-83bb891bb/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-btn"
+          >
+            <AiFillLinkedin />
+          </a>
         </div>
         {/* Button Links */}
         <div className="flex space-x-4 mt-4">
